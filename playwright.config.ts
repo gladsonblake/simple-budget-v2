2f5ef09e05ea7a69331a006ddce1b31a6834053c
Blake Gladson <gladsonblake@gmail.com>
2026-03-28 14:17:55 -0500

    feat: add playwright config and scripts

---
+++ playwright.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, devices } from '@playwright/test'
+
+export default defineConfig({
+  testDir: 'tests/e2e',
+  outputDir: 'tests/e2e/.playwright',
+  use: {
+    baseURL: 'http://localhost:3000',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+  },
+})
